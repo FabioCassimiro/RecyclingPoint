67f1c0910486b645059c942c254a9ccacf0d414e
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-06-04 17:09:36 -0300

    Criação do painel padrao do programa, criação de classe de utilitarios

---
+++ RecyclingPoint/src/br/com/recycling/utils/DefaultComponents.java
@@ -0,0 +1,21 @@
+
+package br.com.recycling.utils;
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author WINDOWS
+ */
+public class DefaultComponents {
+    
+    public static Color primaryColor = new Color(32,178,170);
+    public static Color secundaryColor = new Color(0,139,139);
+    public static Color textColor = new Color(255,255,255);
+    public static Font fontText = new Font("Arial", Font.BOLD, 30);
+    
+    
+    
+}
---
+++ RecyclingPoint/src/br/com/recycling/utils/DefaultPanel.java
@@ -0,0 +1,35 @@
+package br.com.recycling.utils;
+
+import java.awt.event.MouseAdapter;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Fabio Cassimiro
+ */
+
+public class DefaultPanel extends JPanel {
+
+    public DefaultPanel() {
+        setSize(500, 800);
+        setBackground(DefaultComponents.primaryColor);
+        add(buttonClose());
+        
+    }
+    
+    private JLabel buttonClose(){
+        JLabel buttonClose = new JLabel("X");
+        buttonClose.setFont(DefaultComponents.fontText);
+        buttonClose.setBounds(470, 10, 25, 25);
+        buttonClose.setForeground(DefaultComponents.textColor);
+        buttonClose.addMouseListener(new MouseAdapter() {
+             @Override
+             public void mousePressed(java.awt.event.MouseEvent evt) {
+                System.exit(0);
+            }
+        });
+
+        return buttonClose;
+    }
+}
