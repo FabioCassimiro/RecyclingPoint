0139af4b99b3c2a948a7a63203e83c7b93d6c6cd
FabioCassimiro <fabiocassimiro.project@gmail.com>
2020-06-11 17:45:04 -0300

    Remoção de bug e adição de função que atualiza score]

--- RecyclingPoint/src/br/com/recycling/controller/ControllerRecycling.java
+++ RecyclingPoint/src/br/com/recycling/controller/ControllerRecycling.java
@@ -1,4 +1,3 @@
-
 package br.com.recycling.controller;
 
 import br.com.recycling.exception.FieldValueNotInformed;
@@ -9,51 +8,50 @@ import java.util.ArrayList;
 
 /**
  *
- * @author WINDOWS
+ * @author Fabio Cassimiro
  */
 public class ControllerRecycling {
-    
-    ConsultDAO consultDAO = new ConsultDAO();
-    ChangeDAO changeDAO = new ChangeDAO();
+
+    private ConsultDAO consultDAO = new ConsultDAO();
+    private ChangeDAO changeDAO = new ChangeDAO();
     public static ArrayList<String> finalValues = new ArrayList<>();
-    int amount;
-    
-    public void Recycling(String item, String amount) throws FieldValueNotInformed{
+    private int quantity;
+
+    public void Recycling(String item, String quantity) throws FieldValueNotInformed {
         finalValues = consultDAO.valuesItem(item);
-        this.amount = Integer.parseInt(amount);
+        this.quantity = Integer.parseInt(quantity);
         finalValues.set(2, calcScore());
         finalValues.set(3, calcTime());
-        changeDAO.updateScore("SCORE", calcScore());
-        
+        changeDAO.updateScore("SCORE", newScore());
+
     }
-    
-    public void validValue(String amout)throws MinimumAmountOfFieldNotReported{
-        if(amout.equals("0")){
+
+    public void validValue(String amout) throws MinimumAmountOfFieldNotReported {
+        if (amout.equals("0")) {
             throw new MinimumAmountOfFieldNotReported("Unreported quantity");
         }
     }
-    
-    public void validItem(String item) throws FieldValueNotInformed{
-        if(item.equals("Select Item")){
+
+    public void validItem(String item) throws FieldValueNotInformed {
+        if (item.equals("Select Item")) {
             throw new FieldValueNotInformed("Unreported item");
         }
     }
     
-
-    
-    
-    
-    public String calcScore(){
-        return String.valueOf(Integer.parseInt(ConsultDAO.values.get(2))* amount);
+    public String calcScore() {
+        return String.valueOf(Integer.parseInt(ConsultDAO.values.get(2)) * quantity);
     }
-    
-    public String calcTime(){
-        if(ConsultDAO.values.get(3).equals("Indeterminado")){
+
+    public String calcTime() {
+        if (ConsultDAO.values.get(3).equals("Indeterminado")) {
             return "Indeterminado";
-        }else{
-            return String.valueOf(Integer.parseInt(ConsultDAO.values.get(3))* amount);
+        } else {
+            return String.valueOf(Integer.parseInt(ConsultDAO.values.get(3)) * quantity);
         }
     }
     
-    
+    public String newScore() {
+        return String.valueOf(Integer.parseInt(finalValues.get(2)) + Integer.parseInt(ConsultDAO.consultScore()));
+    }
+
 }
